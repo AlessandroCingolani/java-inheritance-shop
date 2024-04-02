63adf7a4413616ccce1b3588d8abef5de98b7b3f
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-04-02 15:53:21 +0200

    Add long and test case 1

---
+++ src/org/experis/shop/Cart.java
@@ -0,0 +1,56 @@
+package org.experis.shop;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Cart {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many products? ");
+        int size = Integer.parseInt(scan.nextLine());
+        Prodotto[] cart = new Prodotto[size];
+
+        for (int i = 0; i < cart.length; i++) {
+            System.out.print("Which product do you want to add:\nPress 1 for: Smartphone\nPress 2 for:Televisore\nPress 3 for:Cuffia\n");
+            int selection = Integer.parseInt(scan.nextLine());
+            System.out.print("Name: ");
+            String name = scan.nextLine();
+            System.out.print("Brand: ");
+            String brand = scan.nextLine();
+            System.out.print("Price: ");
+            String price = scan.nextLine();
+            System.out.print("Vat: ");
+            String vat = scan.nextLine().replaceAll(",",".");
+            switch (selection){
+                case 1 :
+                    System.out.print("IMEI: ");
+                    long imei = Long.parseLong(scan.nextLine());
+                    System.out.print("Memory: ");
+                    int memory = Integer.parseInt(scan.nextLine());
+                    Smartphone smartphone = new Smartphone(name,brand,new BigDecimal(price),new BigDecimal(vat), imei,memory);
+                    cart[i] = smartphone;
+                    break;
+                    case 2 :
+                        System.out.print("Inch: ");
+                        String inch = scan.nextLine();
+                        System.out.print("Is smart? ");
+                        boolean isSmart = scan.nextBoolean();
+                        Televisori television = new Televisori(name,brand,new BigDecimal(price),new BigDecimal(vat),new BigDecimal(inch),isSmart);
+                        cart[i] = television;
+                    break;
+                    case 3 :
+                        System.out.print("Color: ");
+                        String color = scan.nextLine();
+                        System.out.print("Is wireless? ");
+                        boolean isWireless = scan.nextBoolean();
+                        Cuffie headphone = new Cuffie(name,brand,new BigDecimal(price),new BigDecimal(vat),color,isWireless);
+                        cart[i] = headphone;
+                    break;
+            }
+        }
+        System.out.println(Arrays.toString(cart));
+
+
+        scan.close();
+    }
+}
--- src/org/experis/shop/Smartphone.java
+++ src/org/experis/shop/Smartphone.java
@@ -4,11 +4,11 @@ import java.math.BigDecimal;
 
 public class Smartphone extends Prodotto {
     //Attributi
-    public int imei;
+    public long imei;
     public int memory;
 
 
-    public Smartphone(String name, String brand, BigDecimal price, BigDecimal vat,int imei,int memory) {
+    public Smartphone(String name, String brand, BigDecimal price, BigDecimal vat,long imei,int memory) {
         super(name, brand, price, vat);
         this.imei = imei;
         this.memory = memory;
