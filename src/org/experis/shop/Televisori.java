b56dd72c7d7ff9563af6b9af0590c88d11c12ddf
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-04-02 14:48:42 +0200

    Add extends and attributes televisori

--- src/org/experis/shop/Televisori.java
+++ src/org/experis/shop/Televisori.java
@@ -1,4 +1,15 @@
 package org.experis.shop;
 
-public class Televisori {
+import java.math.BigDecimal;
+
+public class Televisori extends Prodotto{
+    //Attributi
+    private BigDecimal inch;
+    private boolean isSmart;
+
+    public Televisori(String name, String brand, BigDecimal price, BigDecimal vat,BigDecimal inch,Boolean isSmart) {
+        super(name, brand, price, vat);
+        this.inch = inch;
+        this.isSmart = isSmart;
+    }
 }
