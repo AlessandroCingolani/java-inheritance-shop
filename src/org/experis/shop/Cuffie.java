156b376d012240d92e6498ecd94f938ec49ba21d
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2024-04-02 14:51:42 +0200

    Add extends and attributes Cuffie

--- src/org/experis/shop/Cuffie.java
+++ src/org/experis/shop/Cuffie.java
@@ -1,4 +1,15 @@
 package org.experis.shop;
 
-public class Cuffie {
+import java.math.BigDecimal;
+
+public class Cuffie extends Prodotto{
+    //Attributi
+    private String color;
+    private boolean isWireless;
+    public Cuffie(String name, String brand, BigDecimal price, BigDecimal vat,String color,boolean isWireless) {
+        super(name, brand, price, vat);
+        this.color = color;
+        this.isWireless = isWireless;
+    }
+    //Attributi
 }
